1b12aead9ad03d20cd47caeee05cb816399ba345
paul-ilies <pauldaniel.ilies@gmail.com>
2022-01-27 18:40:08 +0200

    updated build folder

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const path = require("path")
 const tasksRouter = require("./routers/taskRouter");
 const userRouter = require("./routers/userRouter");
+const { dirname } = require("path");
 
 const app = express();
 
@@ -12,9 +13,8 @@ app.use(morgan("combined"));
 app.use(cors());
 
 
-app.use(express.static(path.join(__dirname, "..", "front-end", "build")))
-
-app.get("/*", (req, res) => {
+app.use("/", express.static(path.join(__dirname, "..", "front-end", "build")))
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "front-end", "build", "index.html"))
 })
 
--- server/db.js
+++ server/db.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, () => {
-            console.log("mongo connected")
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true
         })
+        console.log("Mongo connected")
     } catch (error) {
         console.error(`Error:${error.message}`);
         process.exit(1);
