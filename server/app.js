8c9885397eb9c163dc530eaf0ab277a6e65d1c7b
paul-ilies <pauldaniel.ilies@gmail.com>
2022-01-27 15:00:11 +0200

    deploy to heroku

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path")
 const tasksRouter = require("./routers/taskRouter");
 const userRouter = require("./routers/userRouter");
 
@@ -10,6 +11,11 @@ app.use(express.json());
 app.use(morgan("combined"));
 app.use(cors());
 
+
+app.use("/", express.static("/front-end/build"))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/front-end/build/index.html"))
+})
 
 app.use(tasksRouter)
 app.use(userRouter)
