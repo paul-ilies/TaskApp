86419f4ed1b84edffe32d60634f5d582cccd1de0
paul-ilies <pauldaniel.ilies@gmail.com>
2022-01-28 16:05:20 +0200

    server.js

--- server/server.js
+++ server/server.js
@@ -6,10 +6,12 @@ const path = require("path")
 
 const server = http.createServer(app);
 
-app.get("*", (req, res) => {
+
+app.use(express.static(path.join(__dirname, "..", "front-end", "build")))
+
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "front-end", "build", "index.html"))
 })
-app.use(express.static(path.join(__dirname, "..", "front-end", "build")))
 
 server.listen(process.env.PORT || 5000, () => {
     console.log("Server is running")
